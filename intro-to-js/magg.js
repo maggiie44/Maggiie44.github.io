31399915a3f0ea80b9f42314a36d783e3a6c7d13
kunanon Mahatanasombat <maggiie44@gmail.com>
2022-02-18 14:11:29 +0700

    addjss

--- intro-to-js/magg.js
+++ intro-to-js/magg.js
@@ -1,15 +1,15 @@
-function askMyName() {
-    const name =  prompt("Please enter your name")
+// function askMyName() {
+//     const name =  prompt("Please enter your name")
  
-    if (name != null) {
-       document.getElementById("myname").innerHTML = "My name is " + name;
-    }
- }
+//     if (name != null) {
+//        document.getElementById("myname").innerHTML = "My name is " + name;
+//     }
+//  }
  
- function helloWorld() {
- const h1message =  document.getElementById("myname").innerHTML
-    alert(h1message + 'หลอนๆหมอนอยู่ไหน')
- }
+//  function helloWorld() {
+//  const h1message =  document.getElementById("myname").innerHTML
+//     alert(h1message + 'หลอนๆหมอนอยู่ไหน')
+//  }
 
  function calAge(){
     const year = prompt("Enter your ค.ศ.")
