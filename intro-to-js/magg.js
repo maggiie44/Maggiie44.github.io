673fa04bad56a48309428f57f01e3a013bc2a9d3
kunanon Mahatanasombat <maggiie44@gmail.com>
2022-02-18 14:00:01 +0700

    jsex2

--- intro-to-js/magg.js
+++ intro-to-js/magg.js
@@ -1,12 +1,28 @@
-function askMyName() {
-    const name =  prompt("Please enter your name")
+// function askMyName() {
+//     const name =  prompt("Please enter your name")
  
-    if (name != null) {
-       document.getElementById("myname").innerHTML = "My name is " + name;
-    }
+//     if (name != null) {
+//        document.getElementById("myname").innerHTML = "My name is " + name;
+//     }
+//  }
+ 
+//  function helloWorld() {
+//  const h1message =  document.getElementById("myname").innerHTML
+//     alert(h1message + 'หลอนๆหมอนอยู่ไหน')
+//  }
+
+ function calAge(){
+    const year = prompt("Enter your ค.ศ.")
+    const age = (2022 - year)
+    
+    
+    alert ("แข็งเกร่งมาก นายอยู่ในประเทศนี้มา"+age+ "ปีแล้ว พอได้แล้ว")
  }
- 
- function helloWorld() {
- const h1message =  document.getElementById("myname").innerHTML
-    alert(h1message + 'หลอนๆหมอนอยู่ไหน')
+
+ function loadprofile() {
+   const fname = "kunanon"
+   const provinece = "lossantos"
+      document.getElementById("my-name").innerHTML =fname;
+      document.getElementById("my-province").innerHTML = provinece;
+
  }
